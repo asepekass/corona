3e6288928a45622f4881fb5286c35d1a2e3f6d63
asepekass <58411319+asepekass@users.noreply.github.com>
2020-06-25 19:00:13 +0700

    Update curl.php

--- curl.php
+++ curl.php
@@ -19,40 +19,6 @@ return $output;
 
 }
 
-$data = http_request("https://api.kawalcorona.com/indonesia/provinsi/");
-
-$data = json_decode($data, TRUE);
-
-// echo "<pre>";
-// print_r($data);
-// echo "</pre>";
-
-$jumlah = count($data);
-
-$nomor = 1;
-
-for($i = 0; $i < $jumlah; $i++){
-
-$hasil = $data[$i]['attributes'];
-$pos = hasil['Kasus_Posi'];
-$posri = number_format($pos,0,',','.');
-
-$sem = hasil['Kasus_Semb'];
-$semri = number_format($sem,0,',','.');
-
-$men = hasil['Kasus_Meni'];
-$menri = number_format($men,0,',','.');
-
-?>
-
-<tr>
-<td><??></td>
-<td><?=$hasil['Provinsi']?></td>
-<td><?=$posri?></td>
-<td><?=$semri?></td>
-<td><?=$menri?></td>
-
-</tr>
 
 <?php
 }
