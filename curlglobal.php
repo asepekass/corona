0b1aa0008e5c7d71fa60449c6098fe7508027884
asepekass <58411319+asepekass@users.noreply.github.com>
2020-06-25 19:01:16 +0700

    Update curlglobal.php

--- curlglobal.php
+++ curlglobal.php
@@ -19,43 +19,7 @@ return $output;
 
 }
 
-$data = http_request("https://coronavirus-19-api.herokuapp.com/countries");
 
-$data = json_decode($data, TRUE);
-
-// echo "<pre>";
-// print_r($data);
-// echo "</pre>";
-
-$jumlah = count($data);
-
-$nomor = 1;
-
-for($i = 0; $i < $jumlah; $i++){
-
-$hasil = $data[$i];
-$cas = hasil['cases'];
-$casri = number_format($cas,0,',','.');
-
-$rec = hasil['recovered'];
-$recri = number_format($rec,0,',','.');
-
-$dea = hasil['deaths'];
-$deari = number_format($dea,0,',','.');
-
-
-?>
-
-<tr>
-<td><??></td>
-<td><?=$hasil['country']?></td>
-<td><?=$casri?></td>
-<td><?=$recri?></td>
-<td><?=$deari?></td>
-
-</tr>
-
-<?php
 }
 
 
